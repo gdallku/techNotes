798241b31c10c1d987638a89aebc674951d9c3cd
gdallku <gezim.d@live.com>
2022-12-24 03:37:22 +0100

    chnanges

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {disableReactDevTools} from '@fvilers/disable-react-devtools'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { processResult } from "immer/dist/internal";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if(processResult.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
